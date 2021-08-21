ea7e3b7a4473267881da4d43b9a87cc5714bc2bf
Sotelo <cgonzalezsotelo@gmail.com>
2021-08-20 20:51:51 -0400

    desafio trabajando con props

---
+++ src/components/Hijo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="Hijo">
+    <h2>Lista</h2>
+    <ul>
+      <li v-for="(tarea, index) in tareas" :key="index">
+        {{ tarea }}
+        <button @click="eliminarTarea(index)">Eliminar</button>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "Hijo",
+  props: {
+    tareas: Array
+  },
+  methods: {
+    eliminarTarea(index) {
+      this.$emit("eliminar-tarea", index)
+    }
+  }
+};
+</script>
+<style scoped></style>
---
+++ src/components/Padre.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="Padre">
+    <form id="tareas">
+      <h2>Crar una nueva tarea</h2>
+      <label>Tarea</label>
+      <input
+        type="text"
+        v-model="nuevaTarea"
+        placeholder="Ingresa una nueva tarea"
+      />
+      <button @click.prevent="agregarNuevaTarea">Crear</button>
+    </form>
+    <Hijo :tareas="this.tareas" @eliminar-tarea="eliminarTaeraLista"></Hijo>
+  </div>
+</template>
+
+<script>
+import Hijo from "./Hijo.vue";
+
+export default {
+  name: "Padre",
+  data: () => ({
+    nuevaTarea: "",
+    tareas: []
+  }),
+  components: {
+    Hijo
+  },
+  methods: {
+    agregarNuevaTarea() {
+      this.tareas.push(this.nuevaTarea);
+      this.nuevaTarea = "";
+    },
+    eliminarTaeraLista(index) {
+      this.tareas.splice(index, 1)
+    }
+  }
+};
+</script>
+
+<style scoped></style>
